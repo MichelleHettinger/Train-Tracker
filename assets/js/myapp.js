d510f3e666289335c2b36bac1ceb009deb441993
Michelle Hettinger <Michelle Hettinger>
2016-08-03 15:06:04 -0400

    new row now put in local storage

--- assets/js/myapp.js
+++ assets/js/myapp.js
@@ -1,35 +1,34 @@
 
-//When the submit button is pressed, the values in the input fields are stored in new variables and train count increments by 1
+
+
+if (trainCount == null){
+	var trainCount = 0;
+}
+
+//When the submit button is pressed, the values in the input fields are stored in new variable
+//Then a new row is created with unique identifier (data-train) and text containing the input information
+//The new row is stored in local storage and train count increment by 1
 $('#press-submit').on('click', function() {
 	var newTrainName = $("#add-train").val().trim();
 	var newDestination = $("#add-destination").val().trim();
 	var newFrequency =  $("#add-first-train").val().trim();
-	var newArrival  $("#add-frequency").val().trim();
+	var newArrival =  $("#add-frequency").val().trim();
 	var newMinutes =  $("#press-submit").val().trim();
 
+	var newRow = $("<tr>" +
+	"<td>" + newTrainName + "</td>" +
+	"<td>" + newDestination + "</td>" +
+	"<td>" + newFrequency + "</td>" +
+	"<td>" + newArrival + "</td>" +
+	"<td>" + newMinutes + "</td>" +
+	 "</tr>");
+
+	newRow.attr("data-train", trainCount);
+
+	localStorage.setItem(newRow);
+
 	trainCount++;
+
+	return false;
 })
 
-
-var apple = [1,2];
-
-//Display the information to the table
-for(i=0; i<apple.length; i++){
-	var newRow = $("<tr>");
-
-	var newTrainName = $("<td>");
-	var newDestination = $("<td>");
-	var newFrequency = $("<td>");
-	var newArrival = $("<td>");
-	var newMinutes = $("<td>");
-
-	newTrainName.text(apple[i]);
-	newDestination.text(apple[i]);
-	newFrequency.text(apple[i]);
-	newArrival.text(apple[i]);
-	newMinutes.text(apple[i]);
-
-	newRow.append(newTrainName, newDestination, newFrequency, newArrival, newMinutes);
-
-	$("#good").append(newRow);
-}
