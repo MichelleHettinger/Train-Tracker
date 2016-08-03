93905a213c9bee0662577acc35960ccc2c1f60c0
Michelle Hettinger <Michelle Hettinger>
2016-08-03 14:21:12 -0400

    added function to take input values and store into variables and raise train count by 1

--- assets/js/myapp.js
+++ assets/js/myapp.js
@@ -1,4 +1,14 @@
 
+//When the submit button is pressed, the values in the input fields are stored in new variables and train count increments by 1
+$('#press-submit').on('click', function() {
+	var newTrainName = $("#add-train").val().trim();
+	var newDestination = $("#add-destination").val().trim();
+	var newFrequency =  $("#add-first-train").val().trim();
+	var newArrival  $("#add-frequency").val().trim();
+	var newMinutes =  $("#press-submit").val().trim();
+
+	trainCount++;
+})
 
 
 var apple = [1,2];
